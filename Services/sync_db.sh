d9afae0f66364c0363c8bd52700459cc0341a28c
dominickasanga <dominickasanga@gmail.com>
2022-11-05 22:01:47 +0200

    modified:   .gitignore 	modified:   Services/sync_db.sh

--- Services/sync_db.sh
+++ Services/sync_db.sh
@@ -3,4 +3,4 @@ username="emr_remote_db_backup"
 ip_address="192.168.11.103"
 password="lin@1088"
 
-sshpass -p $password rsync -rav --progress /var/www/EMR-DB-BACKUP/ $username@$ip_address:/var/www/Remote-DB-BacksUps/test_dir/
+sshpass -p $password rsync -rav --log-file=log.txt --progress /var/www/EMR-DB-BACKUP/ $username@$ip_address:/var/www/Remote-DB-BacksUps/test_dir/
